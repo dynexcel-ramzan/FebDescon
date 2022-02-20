e4a141fef8d896e033b79b8360fc021b2a16de7e
dynexcel-ddc1 <ddc1@dynexce.co>
2022-02-20 13:44:49 +0500

    Update account_move.py

--- de_portal_expence/models/account_move.py
+++ de_portal_expence/models/account_move.py
@@ -5,5 +5,7 @@ from odoo import models, fields, api, _
 class AccountMove(models.Model):
     _inherit = 'account.move'
     
+
+
     expense_id = fields.Many2one('hr.expense.sheet', string='Expense Claim')
     
