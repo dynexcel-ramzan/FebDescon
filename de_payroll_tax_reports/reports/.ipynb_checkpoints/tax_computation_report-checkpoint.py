a1db6920234808cb5c914d9e6ea4409c9e2a56c8
dynexcel-ramzan <m.ramzan@dynexcel.co>
2022-03-02 07:11:16 +0000

    update payroll tax reports

--- de_payroll_tax_reports/reports/.ipynb_checkpoints/tax_computation_report-checkpoint.py
+++ de_payroll_tax_reports/reports/.ipynb_checkpoints/tax_computation_report-checkpoint.py
@@ -8,6 +8,8 @@ import time
 from odoo import api, models
 from dateutil.parser import parse
 from odoo.exceptions import UserError
+from datetime import date, datetime, timedelta
+
 
 class PayrollTaxComputation(models.AbstractModel):
     _name = 'report.de_payroll_tax_reports.computation_report'
@@ -17,10 +19,17 @@ class PayrollTaxComputation(models.AbstractModel):
     @api.model
     def _get_report_values(self, docids, data=None): 
         docs = self.env['tax.computation.wizard'].browse(self.env.context.get('active_id'))
-        payslips=self.env['hr.payslip'].search([('employee_id','=',docs.employee_id.id),('date_to','>=',docs.date_from),('date_to','<=',docs.date_to)], order='date_to asc')
+        employee = docs.employee_id
+        date_from = docs.date_from
+        date_to = docs.date_to
+        if data != None:
+            employee = data['employee']
+            date_from = datetime.strptime(str(data['start_date']), "%Y-%m-%d")
+            date_to = datetime.strptime(str(data['end_date']), "%Y-%m-%d")          
+        payslips=self.env['hr.payslip'].search([('employee_id','=',employee.id),('date_to','>=',date_from),('date_to','<=',date_to)], order='date_to asc')
         salary_rules=self.env['hr.salary.rule'].search([('computation_report','=',True)], order='computation_sequence asc')
         pfund_rule=self.env['hr.salary.rule'].search([('pfund_amount','=',True)], limit=1) 
-        contract=self.env['hr.contract'].search([('employee_id','=',docs.employee_id.id),('state','=','open')], limit=1)
+        contract=self.env['hr.contract'].search([('employee_id','=',employee.id),('state','=','open')], limit=1)
 
         pfund_amount = round((contract.wage/12))
         total_amount_list=[]
@@ -73,16 +82,25 @@ class PayrollTaxComputation(models.AbstractModel):
                     'loan_amount':  loan_amount,
                 }) 
             
-        tax_range=self.env['hr.tax.range.line'].search([('year','=',docs.date_to.year)])
-        if payslips: 
-            
-            pfund_amount = (ppfund_amount*12) 
-            exceed_limit=False
-            if   pfund_amount > 150000:
-                pfund_amount = pfund_amount - 150000  
-                exceed_limit = True   
+        tax_range=self.env['hr.tax.range.line'].search([('year','=',date_to.year)])
+        exceed_limit = False
+        if payslips:             
+            if (employee.pf_member == 'yes_with' or employee.pf_member == 'yes_without' and employee.company_id.id==6): 
+                pf=((contract.wage * 9)/100)*12
+            elif (employee.pf_member == 'yes_with' or employee.pf_member == 'yes_without' and employee.company_id.id!=6): 
+                pf=((contract.wage * 6.3)/100)*12
+            apf=0
+            if employee.id==3630:
+                pf=(((contract.wage * 6.3)/100)*11 ) + 19054
+            if(pf>150000):
+                pfund_amount=pf-150000
+                exceed_limit = True 
+ 
             return {
                 'docs': docs,
+                'date_from': date_from,
+                'date_to':  date_to,
+                'employee': employee,
                 'payslips': payslips,
                 'exceed_limit': exceed_limit,
                 'pfund_amount': pfund_amount,
